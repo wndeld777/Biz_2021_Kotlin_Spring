efcd909893036b81d7ea5d606ac972e08a4a2a39
wndeld777 <wndeld777@gmail.com>
2021-10-29 09:45:47 +0900

    READBOOK

--- KSpring03_ReadBook-v2/ReadBook/src/main/kotlin/com/callor/readbook/controller/ReadBookController.kt
+++ KSpring03_ReadBook-v2/ReadBook/src/main/kotlin/com/callor/readbook/controller/ReadBookController.kt
@@ -1,4 +1,18 @@
 package com.callor.readbook.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
 class ReadBookController {
+
+    @RequestMapping(value = ["/",""],method = [RequestMethod.GET])
+    fun home() : String{
+        return "redirect:/insert"
+    }
+    @RequestMapping(value = ["/insert"],method = [RequestMethod.GET])
+    fun insert():String{
+        return "readbook/write.html"
+    }
 }
